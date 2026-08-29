f640841145d3d4b49c9457451109f6c145d097fe
vocuongthinh2@gmail.com <vocuongthinh2@gmail.com>
2026-08-29 14:22:32 +0700

    update category

--- hair-clip-store-be/src/models/Category.ts
+++ hair-clip-store-be/src/models/Category.ts
@@ -5,6 +5,7 @@ export interface ICategory extends Document {
   slug: string
   description?: string
   isActive: boolean
+  imgUrl?: string
   createdAt?: Date
   updatedAt?: Date
 }
@@ -14,6 +15,7 @@ const categorySchema = new Schema<ICategory>(
     categoryName: { type: String, required: true, trim: true },
     slug: { type: String, required: true, unique: true, lowercase: true },
     description: { type: String, default: '' },
+    imgUrl: { type: String, default: '' },
     isActive: { type: Boolean, default: true }
   },
   { timestamps: true }
